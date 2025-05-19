fc864e21203056087e795c9609a7683308af0c5b
matheusf-s <jogandota@gmail.com>
2025-05-19 11:48:14 -0300

    funcional.0

--- main.py
+++ main.py
@@ -4,48 +4,40 @@ import requests
 
 
 try:
-    ask = input("ASK1\t\t\t""Digite a moeda que você deseja converter: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
+    ask = input("Digite a moeda que você deseja converter: " \
+    "\n\033[31mPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur\033[0m").lower()
     url = "https://www.floatrates.com/daily/"+ask+".json"
     retorno = requests.get(url)
     jsonConversão = retorno.json()
+
+
+    try:
+        quant = float(input("Quanto dessa moeda você deseja?"))
+
+
+
+        try:
+            ask2 = input("Digite a moeda que você deseja saber o valor: " \
+            "\n\033[31mPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur\033[0m").lower()
+            print("A conversão de",quant,ask, "resulta em",round(jsonConversão[ask2]['rate'] * quant, 3), ask2, "\t\033[31m(Valor arrendondado!!)\033[0m")
+        except:
+            if ask == ask2:
+                print("Não é possivel realizar a conversão de moedas iguais!")
+            else:
+                print("Insira uma das alternativas possiveis!!!")
+
+
+
+
+
+    except:
+        print("Digite um valor válido!!")
+
+
+
+    
 except:
     print("Digite uma nomenclatura válido!!")
 
 
 
-try:
-    quant = float(input("Quanto dessa moeda você deseja?"))
-except:
-    print("Digite um valor válido!!")
-
-
-    
-try:
-    ask2 = input("ASK2\t\t\t""Digite a moeda que você deseja saber o valor: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
-    print("A conversão de",quant,ask, "resulta em", ask2,round(jsonConversão[ask2]['rate'] * quant, 3), "\t(Valor arrendondado!!)")
-except:
-    print("Insira uma das alternativas possiveis!!!")
-
-
-
-
-
-
-
-
-
-
-
-
-def tamplate():  
-    try:
-        d = input("Digite um valor válido!")
-        url = "https://www.floatrates.com/daily/"+d+".json"
-        retorno = requests.get(url)
-        jsonConversão  = retorno.json()
-
-
-        print(jsonConversão['brl']['rate'])
-    except:
-        print("tente novamente")
-    
