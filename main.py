97757a149b792dd162eda85c7c038896c168f33d
matheusf-s <jogandota@gmail.com>
2025-05-19 11:10:43 -0300

    modelo_base

--- main.py
+++ main.py
@@ -2,32 +2,19 @@
 import requests
 
 
-
 while False:
     ask = input("Digite a moeda que você deseja converter: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
-    #print(ask)
     if ask == "brl" or ask == "usd" or ask == "eur":
-        def url(): 
-                if ask == brl:
-                    url = "https://www.floatrates.com/daily/brl.json"
-                if ask == usd: 
-                    url = "https://www.floatrates.com/daily/usd.json"
-                if ask == eur: 
-                    url = "https://www.floatrates.com/daily/eur.json"
-        ask2 = input("Digite a moeda que você deseja saber o valor: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur")
+        url = "https://www.floatrates.com/daily/"+ask+".json"
+        retorno = requests.get(url)
+        jsonConversão = retorno.json()
+        ask2 = input("Digite a moeda que você deseja saber o valor: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
+        if ask2 == "brl" or ask2 == "usd" or ask2 == "eur":
+            print(jsonConversão[ask2]['rate'])
+        else:
+            print("Insira uma das alternativas possiveis!!!")
     else:
-        print("Insira uma das opções válidas!")
-
-
-    
-while True:
-    ask = input("Digite a moeda que você deseja converter: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
-    url = "https://www.floatrates.com/daily/"+ask+".json"
-    retorno = requests.get(url)
-    jsonConversão = retorno.json()
-    ask2 = input("Digite a moeda que você deseja saber o valor: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur")
-    print(jsonConversão[ask2]['rate'])
-
+        print("Insira uma das alternativas possiveis!!!")
 
 
 
@@ -35,8 +22,14 @@ while True:
 
 
 def tamplate():  
-    url = "https://www.floatrates.com/daily/usd.json"
-    retorno = requests.get(url)
-    jsonConversão  = retorno.json()
+    try:
+        d = input("Digite um valor válido!")
+        url = "https://www.floatrates.com/daily/"+d+".json"
+        retorno = requests.get(url)
+        jsonConversão  = retorno.json()
 
-    print(jsonConversão['brl']['rate'])
+        print(jsonConversão['brl']['rate'])
+    except:
+        print("tente novamente")
+    
+tamplate()
