d8064a1cedda6872292f1328140ac69942cfffd6
matheusf-s <jogandota@gmail.com>
2025-05-19 11:36:24 -0300

    try_except

--- main.py
+++ main.py
@@ -2,19 +2,35 @@
 import requests
 
 
-while False:
-    ask = input("Digite a moeda que você deseja converter: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
-    if ask == "brl" or ask == "usd" or ask == "eur":
-        url = "https://www.floatrates.com/daily/"+ask+".json"
-        retorno = requests.get(url)
-        jsonConversão = retorno.json()
-        ask2 = input("Digite a moeda que você deseja saber o valor: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
-        if ask2 == "brl" or ask2 == "usd" or ask2 == "eur":
-            print(jsonConversão[ask2]['rate'])
-        else:
-            print("Insira uma das alternativas possiveis!!!")
-    else:
-        print("Insira uma das alternativas possiveis!!!")
+
+try:
+    ask = input("ASK1\t\t\t""Digite a moeda que você deseja converter: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
+    url = "https://www.floatrates.com/daily/"+ask+".json"
+    retorno = requests.get(url)
+    jsonConversão = retorno.json()
+except:
+    print("Digite uma nomenclatura válido!!")
+
+
+
+try:
+    quant = float(input("Quanto dessa moeda você deseja?"))
+except:
+    print("Digite um valor válido!!")
+
+
+    
+try:
+    ask2 = input("ASK2\t\t\t""Digite a moeda que você deseja saber o valor: \nPossibilidades:\nReal = brl\nDolar = usd\nEuro = eur").lower()
+    print("A conversão de",quant,ask, "resulta em", ask2,round(jsonConversão[ask2]['rate'] * quant, 3), "\t(Valor arrendondado!!)")
+except:
+    print("Insira uma das alternativas possiveis!!!")
+
+
+
+
+
+
 
 
 
@@ -28,8 +44,8 @@ def tamplate():
         retorno = requests.get(url)
         jsonConversão  = retorno.json()
 
+
         print(jsonConversão['brl']['rate'])
     except:
         print("tente novamente")
     
-tamplate()
